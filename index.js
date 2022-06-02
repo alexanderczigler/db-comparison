cf6d1c5c4116743da3eee1688af154335cb6d067
Alexander Czigler <dev@ilix.se>
2022-06-02 11:12:07 +0200

    chore: couchdb example

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const couchdbAdapter = require("./lib/couchdb-adapter");
 const mongoAdapter = require("./lib/mongo-adapter");
+
+couchdbAdapter
+  .save({ happy: true })
+  .then((data) => {
+    console.log("CouchDB", "Save", data);
+
+    return couchdbAdapter.get(data._id);
+  })
+  .then((data) => {
+    console.log("CouchDB", "Get", data);
+
+    return couchdbAdapter.destroy(data._id, data._rev);
+  })
+  .then((data) => {
+    console.log("CouchDB", "Destroy", data);
+    process.exit(0);
+  });
 
 mongoAdapter
   .save({ meow: "purr" })
-  .then(() => {
-    console.log("saved");
+  .then((data) => {
+    console.log("MongoDB", "Save", data);
 
     return mongoAdapter.get();
   })
